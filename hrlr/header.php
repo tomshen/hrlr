df9b5eeb4082f45aa2eb75cd12291670e607aed5
Tom Shen <tom@shen.io>
2019-07-06 19:42:16 -0400

    Add "footnotes" support to nav

--- hrlr/header.php
+++ hrlr/header.php
@@ -27,18 +27,57 @@
 	<nav id="site-navigation" class="main-navigation">
 		<button class="menu-toggle" aria-controls="main-navigation-menu" aria-expanded="false"></button>
 		<div id="main-navigation-menu">
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-2',
-				'menu_id'        => 'secondary-menu',
-			) );
-			?>
+			<ul class="hrlr-menu">
+				<?php
+					$page_titles = array(
+						"Human Rights Law Review",
+						"HRLR Online",
+						"A Jailhouse Lawyers' Manual",
+						"Trump Human Rights Tracker",
+					);
+					foreach ($page_titles as $index=>$title):
+				?>
+				<li class="hrlr-menu-item" >
+					<a href="<?php echo get_permalink(get_page_by_title($title)); ?>">
+						<?php echo $title; ?>
+						<sup><?php echo ($index + 1); ?></sup>
+					</a>
+				</li>
+				<?php endforeach; ?>
+				<?php
+					$footnotes = array(
+						"The flagship journal, each volume published in three issues every academic year.",
+						"An online forum for rigorous discussion and analysis of current issues in human rights law.",
+						"A handbook of legal rights and procedures designed for use by people in prison.",
+						"A tool to track the Trump administration’s actions and their impact on human rights.",
+					);
+					foreach ($footnotes as $index=>$footnote):
+				?>
+				<li class="hrlr-menu-footnote hrlr-menu-footnote-<?php echo ($index + 1); ?>">
+					<sup><?php echo ($index + 1); ?></sup>
+					<?php echo $footnote; ?>
+				</li>
+				<?php endforeach; ?>
+			</ul>
+			<ul class="hrlr-menu">
+				<?php
+					$page_titles = array(
+						"About",
+						"Submissions",
+						"News",
+					);
+					foreach ($page_titles as $title):
+				?>
+				<li class="hrlr-menu-item" >
+					<a href="<?php echo get_permalink(get_page_by_title($title)); ?>">
+						<?php echo $title; ?>
+					</a>
+				</li>
+				<?php endforeach; ?>
+				<li class="hrlr-menu-item-secondary" >
+					<a href="https://twitter.com/hrlronline">Twitter</a>, <a href="mailto:jrnhum@law.columbia.edu ">Email</a>
+				</li>
+			</ul>
 		</div>
 	</nav><!-- #site-navigation -->
 
